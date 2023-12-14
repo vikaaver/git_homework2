1baafee1cd77d3ddf4e8fdb87683f43f5eb6fadf
Viktoria Averianova <viktoriiaaverianova@gmail.com>
2023-12-13 18:48:27 -0600

    unit commit

---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+    public String brand;
+    public int year;
+
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+}
